7990eca937c64bd29d022897607b06b0cf86b572
raffycastlee <78766072+raffycastlee@users.noreply.github.com>
2024-10-19 19:31:37 -0400

    Points to proper origin

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...deletes) {
+  let res = [];
+  for (let i in arr) {
+    if (deletes.includes(arr[i])) {
+      continue;
+    }
+    res.push(arr[i]);
+  }
+  return res;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+  if (!Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0) {
+    return "ERROR";
+  }
+  let sum = 0;
+  if (start <= end) {
+    for (; start <= end; start++) {
+      sum += start;
+    }
+  } else {
+    for (; end <= start; end++) {
+      sum += end;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
