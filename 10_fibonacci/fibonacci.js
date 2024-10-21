a59fd36c268d14779e6fd86f93d5b39c784fb96b
raffycastlee <78766072+raffycastlee@users.noreply.github.com>
2024-10-21 14:49:40 -0400

    10_fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  n = Number(n);
+  if (n<0) {
+    return "OOPS";
+  }
+  if (n === 0 ) {
+    return 0;
+  }
+  if (n === 1 || n === 2) {
+    return 1;
+  }
+  let left = 1;
+  let right = 1;
+  for (let i = 3; i <= n; i++) {
+    [left,right] = [right,left+right];
+  }
+  return right;
 };
 
 // Do not edit below this line
