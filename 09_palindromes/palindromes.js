f2d16fc968f08a8e8f4344b1822e45ffe73f94e7
raffycastlee <78766072+raffycastlee@users.noreply.github.com>
2024-10-21 14:34:48 -0400

    09_palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str.replaceAll(/[\s\.\!\?,]/g, () => "").toLowerCase();
+  let left = 0;
+  let right = str.length-1;
+  console.log(str);
+  while (left < right) {
+    if (str[left] !== str[right]) { return false; }
+    left++;
+    right--;
+  }
+  return true;
 };
 
 // Do not edit below this line
