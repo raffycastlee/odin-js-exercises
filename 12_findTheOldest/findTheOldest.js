821714ef80e763a3f9b35871a3abbfb905e8631e
raffycastlee <78766072+raffycastlee@users.noreply.github.com>
2024-10-21 15:02:21 -0400

    12_findTheOldest done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  const CURRENT_YEAR = 2024;
+  return people.sort((a,b) => 
+    ((b.yearOfDeath ? b.yearOfDeath : CURRENT_YEAR)-b.yearOfBirth)-((a.yearOfDeath ? a.yearOfDeath : CURRENT_YEAR)-a.yearOfBirth)
+  )[0];
 };
 
 // Do not edit below this line
